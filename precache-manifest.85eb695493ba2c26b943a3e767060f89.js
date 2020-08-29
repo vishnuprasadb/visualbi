e34f9d36bec32d8abc930a0dcbf4dce6368e6498
flo <prasadbajadka@gmail.com>
2020-08-29 19:24:31 +0530

    Updates

---
+++ precache-manifest.85eb695493ba2c26b943a3e767060f89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ca24188c04630084c74927ef3e7fab",
+    "url": "/visualbi/index.html"
+  },
+  {
+    "revision": "30da6f7cc945fe9f751b",
+    "url": "/visualbi/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fcd3a40c798bafd4bea1",
+    "url": "/visualbi/static/js/2.0bbfc58e.chunk.js"
+  },
+  {
+    "revision": "7d2458e1a459d182f8ec17bf9b14e3bf",
+    "url": "/visualbi/static/js/2.0bbfc58e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30da6f7cc945fe9f751b",
+    "url": "/visualbi/static/js/main.5347669d.chunk.js"
+  },
+  {
+    "revision": "240709dac2074bd5b1b8",
+    "url": "/visualbi/static/js/runtime-main.17bfaa31.js"
+  }
+]);
